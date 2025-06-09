59c6496b7a0415caaa298367d2c69f3f9aeb500b
KaKundam <q.hung2306@gmail.com>
2025-06-09 22:20:07 +0700

    upload url picture

--- BE/src/main/java/com/backend/ecommerce/config/ChatFileConfig.java
+++ BE/src/main/java/com/backend/ecommerce/config/ChatFileConfig.java
@@ -2,19 +2,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Path;
-
-import com.backend.ecommerce.config.FileStorageConfig;
-
 @Configuration
 public class ChatFileConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        Path uploadPath = FileStorageConfig.getUploadPath().getParent(); // -> "uploads"
-        String absolutePath = uploadPath.toFile().getAbsolutePath();
+        // Đường dẫn tuyệt đối đến thư mục "uploads"
+        String absolutePath = "D:/NMCNPM/BE/uploads/";
+
+        System.out.println("✅ ChatFileConfig is loaded!");
 
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + absolutePath + "/");
+                .addResourceLocations("file:" + absolutePath);
     }
 }
